08c7a307056f3388133a4f4173019a3ddc8c36d7
greggregbean <90098955+greggregbean@users.noreply.github.com>
2021-10-22 14:12:36 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include "linerfunction.cpp"
+#include "bublesorter.cpp"
+
+int main()
+{
+    char str[MAXLEN] = {'\0'};
+    char* index[MAXLEN] = {'\0'};
+
+    FILE* fp = fopen("mytext.txt", "r");
+    liner(str, index, fp);
+    fclose(fp);
+
+    bublesorter(index);
+    printf("\n");
+    printf("RESULT OF BUBLESORTER: \n");
+    print(index);
+
+    printf("\n");
+
+    revbublesorter(index);
+    printf("\n");
+    printf("RESULT OF REVBUBLESORTER: \n");
+    print(index);
+
+    return 0;
+}
